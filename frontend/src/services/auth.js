670f2bb2061086d6cd24b40b07a39efbb12e90ee
CREVARD <a.gaulin@groupe-aen.info>
2025-06-11 01:01:39 +0200

    ca part

--- frontend/src/services/auth.js
+++ frontend/src/services/auth.js
@@ -1,29 +1,29 @@
 import { supabase } from '../supabaseClient'
 
 // Inscription avec création de profil
-export async function signUpWithProfile({ email, password, firstName, lastName }) {
+export async function signUpWithProfile({ email, password, prenom, nom }) {
   const { data, error } = await supabase.auth.signUp({
     email,
     password,
-  })
-  if (error) return { error }
+  });
+  if (error) return { error };
 
-  const user = data.user
+  const user = data.user;
   if (user) {
     const { error: errorProfile } = await supabase
       .from('profiles')
-      .insert([{ id: user.id, prenom: firstName, nom: lastName }])
-    if (errorProfile) return { error: errorProfile }
+      .insert([{ id: user.id, prenom, nom }]);
+    if (errorProfile) return { error: errorProfile };
   }
-  return { data }
+  return { data };
 }
 
 // Connexion
 export async function signIn({ email, password }) {
-  return await supabase.auth.signInWithPassword({ email, password })
+  return await supabase.auth.signInWithPassword({ email, password });
 }
 
 // Déconnexion
 export async function signOut() {
-  return await supabase.auth.signOut()
+  return await supabase.auth.signOut();
 }
